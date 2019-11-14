967783da0d8c9445a6cf1ce045bf87e0e50463ca
Kosmoastronauta <mateusz_gil@outlook.com>
2019-11-14 21:57:26 +0100

    added getting all books

--- src/main/java/com/kosmoastronauta/demo/controllers/BookController.java
+++ src/main/java/com/kosmoastronauta/demo/controllers/BookController.java
@@ -1,18 +1,34 @@
 package com.kosmoastronauta.demo.controllers;
 
 import com.kosmoastronauta.demo.domain.Book;
+import com.kosmoastronauta.demo.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 public class BookController
 {
+    @Autowired
+    BookService bookService;
+
     @GetMapping
     public ResponseEntity<List<Book>> getAllBooks()
     {
+        List<Book> allBooks;
 
+        try
+        {
+            allBooks = bookService.getAllBooks();
+        }catch(NoSuchElementException e)
+        {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(allBooks, HttpStatus.OK);
     }
 }
--- src/main/java/com/kosmoastronauta/demo/services/BookService.java
+++ src/main/java/com/kosmoastronauta/demo/services/BookService.java
@@ -1,7 +1,27 @@
 package com.kosmoastronauta.demo.services;
 
+import com.kosmoastronauta.demo.domain.Book;
+import com.kosmoastronauta.demo.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class BookService
-{}
+{
+    @Autowired
+    BookRepository bookRepository;
+
+    public List<Book> getAllBooks()
+    {
+        List<Book> books = new ArrayList<>();
+        bookRepository.findAll().forEach(books::add);
+
+        if(books.isEmpty()) throw new NoSuchElementException("There is no books");
+
+        return books;
+    }
+}
