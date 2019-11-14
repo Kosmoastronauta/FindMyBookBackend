da2175d4b2fe6c234a2a5ccd8bb01dd783f9ed0d
Kosmoastronauta <mateusz_gil@outlook.com>
2019-11-14 22:32:57 +0100

    added javafaker dependency

--- src/test/java/com/kosmoastronauta/demo/controllers/BookControllerTest.java
+++ src/test/java/com/kosmoastronauta/demo/controllers/BookControllerTest.java
@@ -1,5 +1,10 @@
 package com.kosmoastronauta.demo.controllers;
 
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.parsing.Parser;
+import com.kosmoastronauta.demo.domain.Book;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -15,6 +20,24 @@ class BookControllerTest
     void getAllBooksWhileThereIsNoBookInDatabase()
     {
         given().when().get(URL + "/getAllBooks/").then().statusCode(HttpStatus.NO_CONTENT.value());
+    }
+
+    @Test
+    public void addNewValidBook() throws JSONException
+    {
+        RestAssured.defaultParser = Parser.JSON;
+        JSONObject request = new JSONObject();
+        request.put("title", "Temp Book2!!!!");
+        request.put("author", "Temp author2!!!!!");
+        request.put("edition", "first2!!!!");
+        request.put("describe", "");
+        request.put("year", 2002);
+
+        Book book = given().contentType("application/json")
+                .body(request.toString())
+                .when().post(URL + "/books/")
+                .then().statusCode(HttpStatus.OK.value())
+                .extract().as(Book.class);
     }
 
 }
